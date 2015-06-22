a5040ded04b764a80a705e39bd025d75007bd30b
rbl219 <e.idas1.tid@telefonica.com>
2015-06-22 06:33:59 -0700

    Removing where clause in csubs query

--- src/lib/mongoBackend/MongoCommonUpdate.cpp
+++ src/lib/mongoBackend/MongoCommonUpdate.cpp
@@ -910,8 +910,6 @@ static bool addTriggeredSubscriptions
   queryPattern.append(condValueQ, attr);
   queryPattern.append(CSUB_EXPIRATION, BSON("$gt" << (long long) getCurrentTime()));
   queryPattern.append(CSUB_SERVICE_PATH, spBson);
-  //RBL
-  //queryPattern.appendCode("$where", function);
 
   // FIXME: condTypeQ, condValueQ and servicePath part could be "factorized" out of the $or clause
   BSONObj query = BSON("$or" << BSON_ARRAY(queryNoPattern << queryPattern.obj()));
